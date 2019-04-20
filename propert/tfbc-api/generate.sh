19429f36d2cd4e53c2b484cc643c9a48d97a3efd
root <root@ip-172-31-22-71.us-east-2.compute.internal>
2019-04-20 17:21:53 +0000

    removing unused folders

--- propert/tfbc-api/generate.sh
+++
@@ -1,9 +0,0 @@
-rm -R crypto-config/*
-
-../bin/cryptogen generate --config=crypto-config.yaml
-
-rm config/*
-
-../bin/configtxgen -profile ProperTOrgOrdererGenesis -outputBlock ./config/genesis.block
-
-../bin/configtxgen -profile ProperTOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID mychannel
